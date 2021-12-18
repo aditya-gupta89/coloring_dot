fcb265987fdcbe02b0a5970711f43ff9bfa3f722
Aditya <adityagupta8023@gmail.com>
2021-12-19 04:54:18 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,32 @@
+(function(){
+let selectedColor='#fff';
+init();
+makeColorPalette();
+function init()
+{
+    let dots=document.querySelector('#dots');
+    for(let i=0;i<dots.children.length;i++)
+    {
+        const dot=dots.children[i];
+        dot.addEventListener('click',changeColor);
+
+    }
+}
+function makeColorPalette()
+{
+    let palette=document.querySelector('#palette');
+    let colors=['red','orange','gold','limegreen','blue'];
+    for(let i=0;i<palette.children.length;i++)
+    {
+        const colorBox=palette.children[i];
+        colorBox.style.backgroundColor=colors[i%colors.length];
+        colorBox.addEventListener('click',function(e){
+            selectedColor=e.target.style.backgroundColor;
+
+        });
+    }
+}
+function changeColor(e){
+    e.target.style.backgroundColor=selectedColor;
+}
+})();
